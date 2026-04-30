409054796ae84cc93046d6bc0c424f9385b3e94d
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2026-04-30 17:33:43 +0200

    feat: error serializer

--- src/serializers/error.ts
+++ src/serializers/error.ts
@@ -3,7 +3,7 @@ import { transferHandlers, type TransferHandler } from 'abslink'
 
 const errorSerializer: TransferHandler<Error, { message: string, name: string, stack?: string }> = {
   canHandle: (value): value is Error => isObject(value) && value instanceof Error && !(proxyMarker in value),
-  serialize: (value) => ({ message: value.message, name: value.name, stack: value.stack }),
+  serialize: (value) => [({ message: value.message, name: value.name, stack: value.stack }), []],
   deserialize: (serialized) => Object.assign(new Error(serialized.message), serialized)
 }
 
